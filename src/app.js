70f23c289603d4522547762067f6949e2a36277a
daramedeiros <daraamedeiros@outlook.com>
2019-11-11 21:59:00 -0300

    interação mongoDB + node.js

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+
+//integração com mongo
+mongoose.connect("mongodb://localhost:27017/reprograma",{ useNewUrlParser: true });
+
+let db = mongoose.connection;
+db.on("erro", console.log.bind(console, "connection error:"))
+db.once("open", function(){
+  console.log("conexão feita com sucesso!")
+})
+
+//rotas
+const index = require("./routes/index")
+const alunas = require("./routes/alunasRoute")
+const professoras = require("./routes/professorasRoute")
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
+app.use("/", index)
+app.use("/alunas", alunas)
+app.use("/professoras", professoras)
+
+module.exports = app
